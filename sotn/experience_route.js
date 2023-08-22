47879d01d3a33954f376ce3986d624170b607a3d
Joshua Carrigan <sestren@comcast.net>
2023-08-21 22:13:33 -0500

    Add checkboxes up to Death Skip

--- sotn/experience_route.js
+++ sotn/experience_route.js
@@ -1,13 +1,62 @@
-var dracula_spawn_pattern = document.querySelector('#dracula-spawn-pattern');
-var first_possible_index = document.querySelector('#first-possible-index');
-var first_possible_seed = document.querySelector('#first-possible-seed');
-var MOD = 0x100000000;
-var spawn_indexes = [
-    81, 210, 339, 468, // first 4 Dracula spawn indexes, 0 candles
-    89, 218, 347, 476, // first 4 Dracula spawn indexes, 1 candle
-    97, 226, 355, 484, // first 4 Dracula spawn indexes, 2 candles
-    105, 234, 363, 492, // first 4 Dracula spawn indexes, 3 candles
-];
 
-experience_route_path.addEventListener('input', function() {
+var url_parameters = new URLSearchParams(window.location.search);
+var inputs = [
+    "entrance__warg1",
+    "zombie_hallway__warg1",
+    "zombie_hallway__warg2",
+    "zombie_hallway__zombie1",
+    "zombie_hallway__warg3",
+    "zombie_hallway__zombie2",
+    "zombie_hallway__warg4",
+    "zombie_hallway__zombie3",
+    "merman_room__bat1",
+    "merman_room__bat2",
+    "merman_room__bat3",
+    "merman_room__bat4",
+    "warg_hallway__warg1",
+    "warg_hallway__warg2",
+    "warg_hallway__warg3",
+]
+
+var xp = 0;
+var level = 1;
+
+function kill() {
+
+}
+
+function refresh(update_url) {
+    xp = 0;
+    level = 1;
+    inputs.forEach((input_id) => {
+        if (document.getElementById(input_id).checked) {
+            xp += 1;
+            if (xp % 4 == 0) {
+                level += 1;
+            }
+        }
+    });
+    console.log("xp: " + xp + ", level: " + level);
+    // Update URL if requested
+    if (update_url) {
+        const url = new URL(window.location);
+        inputs.forEach((input) => {
+            url.searchParams.set(input, document.getElementById(input).checked);
+        })
+        history.replaceState({}, "", url);
+    }
+}
+
+inputs.forEach((input_id) => {
+    if (url_parameters.get(input_id) == null)
+    {
+        url_parameters.set(input_id, 0);
+    }
+    document.getElementById(input_id).checked = url_parameters.get(input_id) == "true";
+    let element = document.getElementById(input_id);
+    element.addEventListener('click', function() {
+        refresh(true);
+    });
 });
+
+refresh(false);
