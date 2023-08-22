d0238aa27bf5b87d2079740e31fb585a1103a1e9
Joshua Carrigan <sestren@comcast.net>
2023-08-21 19:38:22 -0500

    Create experience route page

---
+++ sotn/experience_route.js
@@ -0,0 +1,13 @@
+var dracula_spawn_pattern = document.querySelector('#dracula-spawn-pattern');
+var first_possible_index = document.querySelector('#first-possible-index');
+var first_possible_seed = document.querySelector('#first-possible-seed');
+var MOD = 0x100000000;
+var spawn_indexes = [
+    81, 210, 339, 468, // first 4 Dracula spawn indexes, 0 candles
+    89, 218, 347, 476, // first 4 Dracula spawn indexes, 1 candle
+    97, 226, 355, 484, // first 4 Dracula spawn indexes, 2 candles
+    105, 234, 363, 492, // first 4 Dracula spawn indexes, 3 candles
+];
+
+experience_route_path.addEventListener('input', function() {
+});
