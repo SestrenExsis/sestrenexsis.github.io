d96b8ec75e56bd8e37f347df0f317173bef9e92b
Joshua Carrigan <sestren@comcast.net>
2023-10-20 09:58:23 -0500

    Figure out how to write video to a canvas

--- sotn/run_comparison.js
+++ sotn/run_comparison.js
@@ -1,30 +1,46 @@
 
-var jumplink = document.getElementById('jump');
-
-jumplink.addEventListener("click", function (event) {
+var actionButton = document.getElementById('actionButton');
+actionButton.addEventListener("click", function (event) {
     event.preventDefault();
-    var videoPlayerA = document.getElementById("videoPlayerA");
-    var videoFileA = document.getElementById("videoFileA");
-    console.log(' videoFileA: ', videoFileA.files[0].name);
-    var videoPlayerB = document.getElementById("videoPlayerB");
-    var videoFileB = document.getElementById("videoFileB");
-    console.log(' videoFileB: ', videoFileB.files[0].name);
+    crop();
+    // jump(60);
+}, false);
+
+function jump(time) {
+    console.log("jump");
+    let videoPlayerA = document.getElementById("videoPlayerA");
+    let videoFileA = document.getElementById("videoFileA");
+    console.log('videoFileA: ', videoFileA.files[0].name);
+    let videoPlayerB = document.getElementById("videoPlayerB");
+    let videoFileB = document.getElementById("videoFileB");
+    console.log('videoFileB: ', videoFileB.files[0].name);
     videoPlayerA.play();
     videoPlayerA.pause();
-    videoPlayerA.currentTime = 60;
+    videoPlayerA.currentTime = time;
     videoPlayerA.play();
     videoPlayerB.play();
     videoPlayerB.pause();
-    videoPlayerB.currentTime = 60;
+    videoPlayerB.currentTime = time;
     videoPlayerB.play();
-}, false);
+}
 
-function loadVideo(playerId, fileId)
-{
-    var videoPlayer = document.getElementById(playerId);
-    var videoFile = document.getElementById(fileId);
-    console.log(' videoFile: ', videoFile.files[0].name);
-    var source = document.createElement('source')
+function crop() {
+    console.log("crop");
+    let canvasA = document.getElementById("canvasA");
+    let contextA = canvasA.getContext("2d");
+    let videoPlayerA = document.getElementById("videoPlayerA");
+    contextA.drawImage(videoPlayerA, 0, 0, 320, 240);
+    let canvasB = document.getElementById("canvasB");
+    let contextB = canvasB.getContext("2d");
+    let videoPlayerB = document.getElementById("videoPlayerB");
+    contextB.drawImage(videoPlayerB, 0, 0, 320, 240);
+}
+
+function loadVideo(playerId, fileId) {
+    let videoPlayer = document.getElementById(playerId);
+    let videoFile = document.getElementById(fileId);
+    console.log('videoFile: ', videoFile.files[0].name);
+    let source = document.createElement('source')
     source.src = URL.createObjectURL(videoFile.files[0]);
     videoPlayer.replaceChildren(source);
 }
